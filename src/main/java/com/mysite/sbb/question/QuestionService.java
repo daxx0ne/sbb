fb307388c4f36f9319f2752a3ef0a0ebbf0f6a9c
daxx0ne <jcn07253@gmail.com>
2023-03-20 14:55:16 +0900

    2-15 질문 등록과 폼을 사용하여 입력값을 체크

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
@@ -24,5 +26,12 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
