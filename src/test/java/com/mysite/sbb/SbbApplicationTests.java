90f4678dbf16e318ff8ad8b04a89811d320d55c4
daxx0ne <jcn07253@gmail.com>
2023-03-20 12:44:52 +0900

    2-05 데이터 수정하기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,6 +1,6 @@
 package com.mysite.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
@@ -15,9 +15,11 @@ class SbbApplicationTests {
 
     @Test
     void testJPA() {
-        Question q = this.questionRepository.findBySubjectAndContent(
-                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-        assertEquals(1, q.getId());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
     }
 
 }
